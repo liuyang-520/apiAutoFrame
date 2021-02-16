bf11ee7a3225e8a43076c55ef3dcd31a29314df1
liuyang <liuyang5175@163.com>
2021-02-16 10:04:39 +0800

    修改failpath的路径

--- common/filepath.py
+++ common/filepath.py
@@ -4,6 +4,7 @@
 #Author:liuyang
 import os
 
+
 def filepath(dirname=None, filename=None):
 	return os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+"/{}".format(dirname)+r"/{}".format(filename)
 
